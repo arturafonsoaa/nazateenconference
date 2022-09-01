f8d25d2b3c4eb7d6d22a6e1d0c9f843e8591c74c
Artur Afonso <arturafonso4a@gmail.com>
2022-08-31 23:38:28 -0300

    Banner background

--- resources/views/website/partials/sections/banner.blade.php
+++ resources/views/website/partials/sections/banner.blade.php
@@ -1,4 +1,5 @@
-<section id="section-banner" class="py-40 sm:py-14 ">
+<section id="section-banner" class="py-14 lg:py-32 bg-center bg-cover bg-fixed bg-no-repeat"
+    style="background-image: url('{{ Vite::asset('resources/img/background1.png') }}')">
     <div class="container m-auto flex flex-col items-center">
         <div class="flex gap-x-8 gap-y-4 justify-center mb-8 sm:min-w-full sm:flex-wrap">
             <div class="flex items-start lg:items-center gap-2" data-aos="fade-up">
@@ -20,35 +21,33 @@
                 </span>
             </div>
         </div>
-        <figure class="max-w-4xl m-auto mb-8" data-aos="fade-up" data-aos-delay="300">
+        <figure class="max-w-6xl m-auto mb-8" data-aos="fade-up" data-aos-delay="300">
             <img src="{{ Vite::asset('resources/img/logo-nazateen-conference.png') }}">
         </figure>
         <div class='w-96 py-4 sm:px-4'>
-            <h2 class='text-center text-5xl font-bold text-white mb-4'  data-aos="fade-up" data-aos-delay="400">Faltam</h2>
-            <div class="flex justify-around items-center gap-2 p-4 mb-2"  data-aos="fade-up" data-aos-delay="500">
-                <div
-                    class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
-                    <span id='days' class="text-2xl font-bold mb-2"></span>
+            <h2 class='text-center text-4xl lg:text-5xl font-bold text-white mb-4' data-aos="fade-up" data-aos-delay="400">
+                Faltam
+            </h2>
+            <div class="flex justify-around items-center gap-2 p-4 mb-2" data-aos="fade-up" data-aos-delay="500">
+                <div class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
+                    <span id='days' class="text-xl lg:text-2xl font-bold mb-2"></span>
                     <span class='text-small'>Dias</span>
                 </div>
-                <div
-                    class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
-                    <span id='hours' class="text-2xl font-bold mb-2"></span>
-                    <span class='text-small'>Horas</span>
+                <div class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
+                    <span id='hours' class="text-xl lg:text-2xl font-bold mb-2"></span>
+                    <span class='text-small'>Hrs</span>
                 </div>
-                <div
-                    class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
-                    <span id='mins' class="text-2xl font-bold mb-2"></span>
-                    <span class='text-small'>Minutos</span>
+                <div class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
+                    <span id='mins' class="text-xl lg:text-2xl font-bold mb-2"></span>
+                    <span class='text-small'>Mins</span>
                 </div>
-                <div
-                    class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
-                    <span id='secs' class="text-2xl font-bold mb-2"></span>
-                    <span class='text-small'>Segundos</span>
+                <div class='py-4 px-6 bg-black/[.4] text-white flex flex-col items-center justify-center'>
+                    <span id='secs' class="text-xl lg:text-2xl font-bold mb-2"></span>
+                    <span class='text-small'>Segs</span>
                 </div>
             </div>
         </div>
-        <div class="text-center"  data-aos="fade-up" data-aos-delay="600">
+        <div class="text-center" data-aos="fade-up" data-aos-delay="600">
             <a href="#section-ticket" class="bg-neon-pink rounded-lg py-2 px-8 text-white uppercase">
                 Comprar Ingresso
             </a>
