07561efbd9f38937489c57c2cecb896c8093d33c
Artur Afonso <arturafonso4a@gmail.com>
2022-09-05 12:06:11 -0300

    Table responsive fixes

--- resources/views/admin/admin-user/index.blade.php
+++ resources/views/admin/admin-user/index.blade.php
@@ -20,25 +20,27 @@
                 </div>
                 <div class="card">
                     <div class="card-body">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Nome</th>
-                                    <th>E-mail</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($users as $user)
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
                                     <tr>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>
-                                        </td>
+                                        <th>Nome</th>
+                                        <th>E-mail</th>
+                                        <th></th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
