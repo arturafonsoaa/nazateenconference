e377facc936582d7503786742a4b1dfe7cbdca45
Artur Afonso Amaro Araújo <arturafonso4a@gmail.com>
2022-09-26 23:08:18 -0300

    Removing sponsors section

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -10,9 +10,9 @@
         @include('website.partials.sections.speakers')
         @include('website.partials.sections.schedule')
         @include('website.partials.sections.ticket')
-        @include('website.partials.sections.sponsors')
-        @include('website.partials.sections.call-to-action')
+        {{-- @include('website.partials.sections.sponsors') --}}
         @include('website.partials.sections.testimonials')
+        @include('website.partials.sections.call-to-action')
         @include('website.partials.sections.gallery')
         @include('website.partials.sections.form')
     </main>
